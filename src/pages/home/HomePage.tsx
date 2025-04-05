65389164de923989a96efb9647fac2efa30f6e37
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-04-05 23:23:01 +0300

    upd home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import styles from './HomePage.module.css';
 import Layout from '../../components/layout/Layout';
+import styles from './HomePage.module.css';
 
-const HomePage: React.FC = () => {
+const HomePage = () => {
   return (
     <Layout>
-      <section className={styles.start}>
+      <section className={styles.hero}>
         <div className={styles.content}>
           <h1 className={styles.title}>FEMINE</h1>
-          <p className={styles.description}>сообщество представительниц разных течений феминизма</p>
+          <p className={styles.description}>сообщество для представительниц разных течений феминизма</p>
           <span></span>
           <p className={styles.description}>оффлайн в Москве | онлайн по всему миру</p>
         </div>
-        
+      </section>
+      <section className={styles.points}>
+        <div className={styles.content}>
+
+        </div>
       </section>
 
     </Layout>
