e8fe7fad8deb58f1f7a90d0a672c3b80b66a9ef5
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-04-08 17:13:05 +0300

    add text section

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,23 +1,37 @@
+import { useState } from 'react';
 import Layout from '../../components/layout/Layout';
 import styles from './HomePage.module.css';
+import TextSection from '../../components/textSection/TextSection';
 
 const HomePage = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsClicked(true);
+    // Здесь может быть логика для обработки клика
+    console.log('Кнопка "Присоединиться" была нажата');
+    setTimeout(() => setIsClicked(false), 300); // Сбрасываем состояние через 300мс
+  };
+
   return (
     <Layout>
       <section className={styles.hero}>
         <div className={styles.content}>
-          <h1 className={styles.title}>FEMINE</h1>
-          <p className={styles.description}>сообщество для представительниц разных течений феминизма</p>
+          <div className={styles.call}>
+            <h1 className={styles.title}>FEMINE</h1>
+            <button 
+              className={`${styles.buttonCall} ${isClicked ? styles.clicked : ''}`}
+              onClick={handleButtonClick}
+            >
+              присоединиться
+            </button>
+          </div>
+          <p className={styles.description}>сообщество для&nbsp;представительниц разных течений феминизма</p>
           <span></span>
-          <p className={styles.description}>оффлайн в Москве | онлайн по всему миру</p>
+          <p className={styles.description}>оффлайн в&nbsp;Москве | онлайн по&nbsp;всему миру</p>
         </div>
       </section>
-      <section className={styles.points}>
-        <div className={styles.content}>
-
-        </div>
-      </section>
-
+      <TextSection />
     </Layout>
   );
 };
