6bc05ba6eb31e02856f10ab74ff1f6a190105f11
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-05-11 14:27:58 +0300

    create TabBar

---
+++ src/components/community/tabbar/Tabbar.tsx
@@ -0,0 +1,33 @@
+import { FC, useState } from "react";
+import { Tab } from "../../../utils/types";
+import styles from './Tabbar.module.css';
+
+type TabBarProps = {
+    onTabChange: (tabId: string) => void;
+    tabs: Tab[];
+}
+
+const TabBar: FC<TabBarProps> = ({ onTabChange, tabs }) => {
+    const [activeTab, setActiveTab] = useState<string>(tabs[0].id);
+
+    const handleClick = (tabId: string) => {
+        setActiveTab(tabId);
+        onTabChange(tabId);
+    };
+
+    return (
+        <ul className={styles.tab}>
+            {tabs.map(tab => (
+                <li
+                    key={tab.id}
+                    className={`${styles.tabItem} ${activeTab === tab.id ? styles.active : ""}`}
+                    onClick={() => handleClick(tab.id)}
+                >
+                    {tab.label}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default TabBar;
