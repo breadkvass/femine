7abbefff56df8d95d57f56a1f71f3db85941f2cc
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-05-11 13:03:30 +0300

    add api with redux

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,3 +5,38 @@ export type TRoute = {
     label: string;
 }
 
+export type Event = {
+  id: string;
+  title: string;
+  description: string;
+  dateTime: string;
+  format: string;
+}
+
+export type User = {
+  id: string;
+  email: string;
+  username: string | null,
+  role: UserRole,
+  accessToken: string;
+  refreshToken: string;
+}
+
+export type UserRole = 'ADMIN' | 'USER';
+
+// ДЛЯ API
+
+export type UserData = {
+  email: string;
+  password: string;
+  username?: string;
+  role: string;
+}
+
+export type EventData = {
+  title: string;
+  description: string;
+  dateTime: string; // ISO-строка
+  format: 'ONLINE' | 'OFFLINE' | '';
+  token: string;
+}
