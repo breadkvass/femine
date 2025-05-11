955ad92ff98739371633bd39b48278cd576b26d0
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-05-11 13:03:45 +0300

    add redux provider to main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { RouterProvider } from "react-router-dom";
 import router from "./App";
 import "./index.css";
 import { ModalContextProvider } from "./hooks/useModal/useModalProvider";
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ModalContextProvider>
-      <RouterProvider router={router} />
-    </ModalContextProvider>
+    <Provider store={store}>
+      <ModalContextProvider>
+        <RouterProvider router={router} />
+      </ModalContextProvider>
+    </Provider>
   </React.StrictMode>
 );
